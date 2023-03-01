e3895fd36c7c49276a4cde91e980404584ae5c7a
faltovaj <jana.faltova@cern.ch>
2023-03-01 07:11:09 +0100

    trida pes

---
+++ Lekce3_tridy/pes.py
@@ -0,0 +1,25 @@
+class Pes():
+	""" Trida psu """
+
+	""" Zvuk psa """
+	zvuk = 'Haf'
+
+	def __init__(self, jmeno, druh):
+		self.jmeno = jmeno
+		self.druh = druh
+
+    def __str__(self):
+    	""" Vypis pro print """
+        return f'{self.jmeno} {self.druh}'
+
+    def __repr__(self):
+    	""" Reprezentace tridy """
+        return f'Pes({self.jmeno} {self.druh})'
+
+    def __eq__(self, other):
+        """ Definice rovnosti instanci (self a other) """
+        return self.druh == other.druh
+
+	def zastekej(self):
+		""" Funkce pro psi stekani """
+		print(f'{self.zvuk}!')
