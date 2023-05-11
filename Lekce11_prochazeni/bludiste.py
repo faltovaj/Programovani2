ce6eb7fc91af1a703ffa11b855800312f137f77a
faltovaj <jana.faltova@cern.ch>
2023-05-11 10:08:56 +0200

    bludiste update

--- Lekce11_prochazeni/bludiste.py
+++ Lekce11_prochazeni/bludiste.py
@@ -16,47 +16,63 @@ XX...C..
 .XX...X.
 """
 
-
 from collections import deque
 
 class Bludiste:
 	def __init__(self):
 		# Ctvercove bludiste
-		self.pole = []
+		self.mapa = []
 		self.rozmer = 0
 
-	def nacteni(self, n):
+	def nacti(self, n):
 		# Muzete si prepsat na nacitani ze souboru
 		self.rozmer = n
 		for i in range(n):
 			line = input()
-			self.pole.append(list(line))
+			self.mapa.append(list(line))
 
-	def najdi_pocatek(self):
+	def start(self):
 		for i in range(self.rozmer):
 			for j in range(self.rozmer):
-				if self.pole[i][j]=='S':
+				if self.mapa[i][j]=='S':
 					return (i,j)
 
-	def najdi_cestu(self, s1, s2):
-		# Fronta na prochazeni do sirky (deque umoznuje rychle odebirani zleva)
-		fronta = deque()
-		fronta.append(s1,s2)
-		# Oznacime navstivene policko
-		self.pole[s1][s2] = 'o'
-		cil = False
+	def cesta(self, r, s):
+		# Pomocna funkce: vraci souradnice ciloveho policka
+		def cil():
+			for i in range(self.rozmer):
+				for j in range(self.rozmer):
+					if self.mapa[i][j] == 'C':
+						return (i, j)
 
-		while fronta and not cil:
-            r, s = fronta.popleft()
-            """
-            TODO: 
-            	- vyzkousejte mozne tahy
-            	- pokud jsme na policku jeste nebyli, oznacte ho, pridejte do fronty
-            	- pokud je policko cilove, ukoncime prochazeni
-            	- pridejte pocitani tahu
+		# Mozne tahy
+		tahy = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+		# Vzdalenost pole od startu
+		vzdalenost = [[-1]*self.rozmer for _ in range(self.rozmer)]
+		# Fronta na prochazeni do sirky
+		# (deque umoznuje rychle a snadne odebirani zleva)
+		fronta = deque()
+		# Pridame prvni bod do fronty
+		fronta.append((r,s))
+		# Vzdalenost v pocatku je 0
+		vzdalenost[r][s] += 1
+		# Souradnice cile
+		c1, c2 = cil()
+		print('Souradnice ciloveho policka: ', c1, c2)
+
+		while fronta:
+			"""
+			TODO:
+			- Odeberte prvek z fronty
+			- Vyzkousejte mozne tahy: 
+				- zkontrolujte, jestli nejste v cili, 
+				pokud ano vratte vzdalenost od startu a je hotovo
+				- pokud jste na policku jeste nebyli, oznacte si ho,
+				ulozte si vzdalenost od startu a pridejte bod do fronty 
 			"""
 
 b = Bludiste()
-b.nacteni(8)
-s1, s2 = b.najdi_pocatek()
-print(s1, s2)
+b.nacti(8)             # Ctvercova sit 8x8
+s1, s2 = b.start()
+print("Souradnice startovniho policka: ", s1, s2)
+#print("Pocet kroku:" , b.cesta(s1, s2))
